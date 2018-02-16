c11882bfd465648d95ca98da8f9b1f094c621127
Kabolas <suri.mur.bry@gmail.com>
2018-02-16 07:18:45 +0100

    modification du script, le  chatbot peur repondre a trois question: qui est le pere de barristan, ses surnoms et ses titres

--- test1.lua
+++ test1.lua
@@ -66,9 +66,35 @@ for line in io.lines("Lexiques/Organizations.txt") do
 		end
 	end
 pipe:lexicon("#Organization", lexo)
+pipe:lexicon("#Possessif", {"her","his","theirs"})
+pipe:lexicon("#Demonstratif", {"her","him","them"})
+pipe:lexicon("#Pronoun", {"she","he","they"})
+pipe:lexicon("#Det", {"the"})
+--Pattern Definitions
 
 pipe:pattern([[
 		[#Character #Title?? (#FirstName|#NickName) (#House|#SurName)?]
+	]])
+pipe:pattern([[
+		[#Person (Who|who) #POS=VRB]
+	]])
+pipe:pattern([[
+		[#Location (Where|where) #w. from]
+	]])
+pipe:pattern([[
+		[#Co (to|To) #Character]
+	]])
+pipe:pattern([[
+		[#Subject #POS=VRB #Character #POS=VRB?]
+	]])
+pipe:pattern([[
+		[#Possess ((#Character "'" s | #Possessif) #w)|(#w of #Character)]
+		]])
+pipe:pattern([[
+		[#Unprecised What (can|do) you (tell me|know) about #Character]
+		]])
+pipe:pattern([[
+		[#Pers #Character|#Demonstratif|#Possessif|#Pronoun]
 ]])
 tags ={
 	["#Castle"] = "cyan",
@@ -83,26 +109,32 @@ tags ={
 	["#SurName"] = "green"
 }
 state ={}
-dofile("BD_Characters_exemple.lua")
+state["#Character"] = {}
+state["#Character"]["Subject"]= ""
+state["#Character"]["Co"] = ""
+state["#Unprecised"] = false
+--dofile("BD_Characters_exemple.lua")
+pipe:lexicon("#Relation", {"father", "mother"})
+Characters = {}
+Characters["Barristan"] ={}
+Characters["Barristan"]["Name"]={}
+Characters["Barristan"]["Name"]["Firstname"]="Barristan"
+Characters["Barristan"]["Name"]["Surname"]="Selmy"
+Characters["Barristan"]["Relation"]={}
+Characters["Barristan"]["Relation"]["father"]={} 
+Characters["Barristan"]["Relation"]["father"]["FirstName"] = "Lyonel"
+Characters["Barristan"]["Relation"]["father"]["Surname"] = "Selmy"
+Characters["Barristan"]["Aliases"] = {}
+Characters["Barristan"]["Aliases"] = {"Barristan the Bold", "Arstan Whitebeard", "Ser Grandfather", "Barristan the Old", "Old Ser"}
+Characters["Barristan"]["titles"] ={}
+Characters["Barristan"]["titles"]["current"] = {"Ser", "Hand of the Queen", "Lord Commander of the Queensguard of Queen Daenerys Targaryen"}
+Characters["Barristan"]["titles"]["former"] = {"Lord Commander of the Kingsguard"}
+
+pipe:lexicon("#QuestionMark", {"Who", "What", "Which", "Where", "When", "How", "Why"})
+pipe:lexicon("#Aliases", {"called", "known as", "Alias", "NickName"})
 
 quitting = false
---fonction de chargement de la table voulue en fonction du tag voulu depuis la reponse et la base de donnée extraite de la reponse
-function chargefromAnswer(pipedanswer, tabletofill, tag, databaseanswer)
-	local ind =1
-	while pipedanswer[tag][ind] ~=nil do
-		tabletofill[ind] = ""
-		local index =1
-		while pipedanswer[tag][ind][index] ~=nil do
-			local wrdex = pipedanswer[tag][ind][index]
-			if wrdex ~= pipedanswer[tag][ind][index-1] then
-				tabletofill[ind] = tabletofill[ind].." ".. databaseanswer[wrdex]
-			end
-			index = index+1
-		end
-		ind = ind +1
-	end
-end
---fonction de traitement de la reponse, elle ne rempli que les personnages pour le moment
+
 function answerTreatment (context, status, answertotreat)
 	context["pipedanswer"] = pipe(answertotreat:gsub("%p", " %0 "))
 	context["base"] = {}
@@ -111,38 +143,83 @@ function answerTreatment (context, status, answertotreat)
 		local k=1
 		print(context["pipedanswer"])
 		if #context["pipedanswer"]["#Character"] ~= 0 then
-		status["#Character"] = {}
-		context["#Character"] = {}
-		status["#Character"]["list"]= {}
-		context["#Character"]["list"] = {}
-		status["#Character"]["activated"] = true
-		chargefromAnswer(context["pipedanswer"], context["#Character"]["list"], "#Character", context["base"])
+			context["#Character"] = {}
+			context["#Character"]["list"] = {}
+			status["#Character"]["activated"] = true
+			chargefromAnswer(context["pipedanswer"], context["#Character"]["list"], "#Character", context["base"], status["#Character"]["list"])
 		end
 		return false
 	else		
 		return true
 	end
 end
+function InfoRetrieval()
 
+end
+contextp = nil
 print([[Hello, i'm AskA.S.O.I.A.F., your chatbot dedicated to A Song Of Ice And Fire.]])
 print([[What can i do for you today ?]])
 while quitting ~= true do
 	local context = {}
 	local interrogation =false
+	local Charac = nil
+	local poswat = nil
+	reply = ""
+	base = {}
 	answer = io.read()
-	interrogation = answerTreatment(context, state, answer)
-	quitting = context["pipedanswer"]["#Quit"] ~= 0
-	if interrogation~=true then
-		if context["#Character"]~=nil then
-			print ("you asked about "..table.concat(context["#Character"]["list"], ", "))
-			
+	piped = pipe(answer:gsub("%p", " %0 "))
+	
+	if #piped["#Character"] == 0 then
+		if #piped["#Pers"] == 0 then
+			print ("Who are you talking about ?")
 		else
-			print("i can only provide information about characters, and you asked about none of which i know. I'm sorry, i can not answer this question")
+			Charac = contextp
 		end
-		
+	else
+		Charac = piped:tag2str("#Character")[1]
+		contextp = Charac
 	end
-
+	if #piped["#Possess"]>0 then
+		local pos = piped:tag2str("#Possess")[1]
+		if pos:find("of") then
+			poswat = pos:sub(1, pos:find("of")-1):gsub("%s", "")
+			print(wat)
+		else
+			if pos:find("' s") then
+			t= ""..pos:find("' s")
+			print (pos:find("' s")+0)
+				poswat = pos:sub(pos:find("' s")+3):gsub("%s", "")
+				print(wat)
+			else
+				if #piped["#Possessif"] ~=0 then
+					poswat = pos:sub(pos:find(piped:tag2str("#Possessif")[1])+3):gsub("%s","")
+				end
+			end
+		end
+		reply = pos.." "
+		reply = reply .. piped:tag2str("#POS=VRB")[1].." "
+	end
+	 -- print (Charac)
+	if Charac then
+		if piped:tag2str("#QuestionMark")[1]:lower() == "who" then 
+			
+			reply = reply .. Characters[Charac]["Relation"][poswat]["FirstName"].." ".. Characters[Charac]["Relation"][poswat]["Surname"].."."
+			reply = reply:gsub("( )(%p )", "%2") 
+		end
+		if piped:tag2str("#QuestionMark")[1]:lower() == "how" then
+			if #piped["#Aliases"] ~= 0 then
+				 reply = reply .. table.concat(Characters[Charac]["Aliases"],", ")
+			end
+		end
+		if piped:tag2str("#QuestionMark")[1]:lower() == "what" then
+			
+			reply = reply .. table.concat(Characters[Charac][poswat]["current"], ", ")
+		end
+	end
+	print (reply)
 end
 
-print("I'm glad that we could have a talk.")print("See you later.")print("And remember, Winter is Coming.")
+print([[I'm glad that we could have a talk.
+See you later.
+And remember, Winter is Coming.]])
 	
